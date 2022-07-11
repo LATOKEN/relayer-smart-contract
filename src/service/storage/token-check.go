22edcba8b1020af1196a1f1b96d517b3a95eea85
arout2 <aditya.rout@latoken.com>
2022-07-11 16:11:15 +0530

    added token check updation

--- src/service/storage/token-check.go
+++ src/service/storage/token-check.go
@@ -9,9 +9,17 @@ func (d *DataBase) SaveTokenChecks(checks []interface{}) {
 			ResourceID:       checkMap["resource_id"].(string),
 			Amount:           int64(checkMap["amount"].(float64)),
 		}
-		if err := d.saveTokenCheck(&check); err != nil {
-			continue
+		_, err := d.FetchSpecificTokenCheck(check.OriginChain, check.DestinationChain, check.ResourceID)
+		if err != nil {
+			if err := d.saveTokenCheck(&check); err != nil {
+				continue
+			}
+		} else {
+			if err := d.updateSpecificTokenCheck(check); err != nil {
+				println("error updating token check")
+			}
 		}
+
 	}
 }
 
@@ -20,6 +28,13 @@ func (d *DataBase) FetchSpecificTokenCheck(origin, dest, resourceID string) (tc 
 		return tc, err
 	}
 	return tc, nil
+}
+
+func (d *DataBase) updateSpecificTokenCheck(tc TokenCheck) error {
+	if err := d.db.Model(TokenCheck{}).Where("origin_chain = ? and destination_chain = ? and resource_id = ?", tc.OriginChain, tc.DestinationChain, tc.ResourceID).Updates(tc).Error; err != nil {
+		return err
+	}
+	return nil
 }
 
 func (d *DataBase) saveTokenCheck(tc *TokenCheck) error {
